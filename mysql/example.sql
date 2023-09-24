49b4993bbc6fdb23272438121d772a322fb735dc
cunlim <zangcunlim@gmail.com>
2023-09-25 01:24:02 +0900

    mysql create user, db no mysql_native_password

--- mysql/example.sql
+++ mysql/example.sql
@@ -1,5 +1,5 @@
 -- create user, db {
-CREATE USER '{username}'@'%' IDENTIFIED WITH mysql_native_password BY '{password}';
+CREATE USER '{username}'@'%' IDENTIFIED BY '{password}';
 CREATE DATABASE {db_name};
 GRANT ALL PRIVILEGES ON {db_name}.{table_name} TO {username}@'%';
 -- create user, db }
